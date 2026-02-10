382c10e8a524e76f9165ca7a26dd0a6110e748b0
adnanahmad55 <adnanahmed5575@gmail.com>
2026-02-10 12:48:17 +0530

    dynamic eu countries

--- app/bd/BDHomeClient.jsx
+++ app/bd/BDHomeClient.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import React from 'react'
 import { useLang } from '../components/LanguageProvider';
 import { dictionary } from '../lib/i18n';
 import BannerForm from '../components/BannerForm';
