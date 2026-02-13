aafb47003f9517de1aabab2b5017e206670d9d42
adnanahmad55 <adnanahmed5575@gmail.com>
2026-02-13 11:01:52 +0530

    uk  resolved

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: "export", // removed
+
+  async redirects() {
+    return [
+      {
+        source: '/bd/sportsbook-software-development-asia', // ❌ पुराना URL
+        destination: '/bd/sportsbook-software-development-bd', // ✅ नया URL (जहाँ भेजना है)
+        permanent: true, // 301 Redirect (SEO के लिए बेस्ट)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
